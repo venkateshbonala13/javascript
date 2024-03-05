b97517ec86be040695ca4becf48bfb39769588a0
venkateshbonala13 <venkateshbonala13@gmail.com>
2024-03-05 15:31:39 -0600

    loops

--- loops.js
+++ loops.js
@@ -60,3 +60,17 @@ function fibonacci(term){
     
 }
 fizzbuzz(100)
+
+//Printing Pattern
+
+var rows = 5;
+for(let i = 0; i < rows; i++) {
+    let row = "";
+    for(let j = 0; j < rows; j++) {
+        if(i + j < rows - 1)
+            row += "+ ";
+        else
+            row += "- ";
+    }
+    console.log(row);
+}
